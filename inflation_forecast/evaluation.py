6ca03f2bacc5dc377a9a66661cfcf5dd2efc3c2c
Yuhao Huo <martinyuhaohuo@gmail.com>
2026-03-14 22:08:57 +0000

    update Dlearner

--- inflation_forecast/evaluation.py
+++ inflation_forecast/evaluation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
-from inflation_forecast.model import create_MLP, create_RNN, create_data_loaders, nn_training, make_predict
+from inflation_forecast.model import create_MLP, create_RNN, create_DL, create_data_loaders, nn_training, make_predict
 
 def compute_MSE(prediction, target):
     target = target.to_numpy()
@@ -9,7 +9,7 @@ def compute_MSE(prediction, target):
     RMSE = MSE**0.5
     return MSE, RMSE
 
-def cross_validation_loss(model, model_type, x_train, y_train, n_split = 5, batch_size = None, epochs = None, lr = None, nodes = None, hidden_size = None, window_size = None):
+def cross_validation_loss(model, model_type, x_train, y_train, n_split = 5, batch_size = None, epochs = None, lr = None, nodes = None, hidden_size = None, window_size = None, individual = None, kernel_size = None):
     time_series_cv = TimeSeriesSplit(n_splits = n_split)
     loss_list = []
     for train_index, val_index in time_series_cv.split(x_train):
@@ -31,6 +31,12 @@ def cross_validation_loss(model, model_type, x_train, y_train, n_split = 5, batc
             RNN = create_RNN(x_tra, y_tra, hidden_size, nodes)
             _, _ = nn_training(RNN, train_loader, test_loader, epochs, lr, print_error = False)
             prediction = make_predict(RNN, x_tra, x_val, True, window_size)
+        elif model_type == "DL":
+            train_loader, test_loader = create_data_loaders(x_tra, y_tra, x_val, y_val, batch_size, window_size)
+            DL = create_DL(x_tra, y_tra, window_size, individual, kernel_size)
+            _, _ = nn_training(DL, train_loader, test_loader, epochs, lr, print_error = False)
+            prediction = make_predict(DL, x_tra, x_val, True, window_size)
+
         
         MSE = ((prediction - y_val.to_numpy())**2).mean()
         loss_list.append(MSE)
--- inflation_forecast/model.py
+++ inflation_forecast/model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import torch
 from torch import nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset, Dataset
 
@@ -88,12 +89,12 @@ def create_data_loaders(x_train, y_train, x_test, y_test, batch_size, window_siz
     else:
         train = WindowData(X_train, Y_train, window_size)
         test = WindowData(X_test, Y_test, window_size)
-    train_loader = DataLoader(train, batch_size = batch_size, shuffle = True)
-    test_loader = DataLoader(test, batch_size = batch_size, shuffle = True)
+    train_loader = DataLoader(train, batch_size = batch_size, shuffle = False)
+    test_loader = DataLoader(test, batch_size = batch_size, shuffle = False)
     return train_loader, test_loader
 
 
-def compute_accuracy(model, data_loader):
+def compute_accuracy(model, data_loader, DL = False):
     criterion = nn.MSELoss(reduction="sum")
     model.eval()
     with torch.no_grad():
@@ -102,14 +103,17 @@ def compute_accuracy(model, data_loader):
         for X, Y in data_loader:
             X, Y = X.to(DEVICE), Y.to(DEVICE)
             outputs = model(X)
-            loss = criterion(outputs, Y)
+            if DL:
+                loss = criterion(outputs, Y.unsqueeze(-1))
+            else:
+                loss = criterion(outputs, Y)
             total_loss += loss.item()
             total_element += Y.numel()
         average_MSE = total_loss/total_element
     return average_MSE
 
 
-def nn_training(model, train_loader, test_loader, epochs, learning_rate, print_error = False):
+def nn_training(model, train_loader, test_loader, epochs, learning_rate, print_error = False, DL = False):
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
 
@@ -122,15 +126,18 @@ def nn_training(model, train_loader, test_loader, epochs, learning_rate, print_e
 
         for batch, (X, Y) in enumerate(train_loader):
             outputs = model(X.to(DEVICE))
-            loss = criterion(outputs, Y.to(DEVICE))
+            if DL:
+                loss = criterion(outputs, Y.to(DEVICE).unsqueeze(-1))
+            else:
+                loss = criterion(outputs, Y.to(DEVICE))
             optimizer.zero_grad()  # clear gradients before each backward (otw. they accumulate)
             loss.backward()        # fill gradients for all parameters with requires_grad=True
             optimizer.step()       # update parameters using those gradients
             MLP_loss_list.append(loss.item())
 
-        train_loss = compute_accuracy(model, train_loader)
+        train_loss = compute_accuracy(model, train_loader, DL)
         train_loss_list.append(train_loss)
-        test_loss = compute_accuracy(model, test_loader)
+        test_loss = compute_accuracy(model, test_loader, DL)
         test_loss_list.append(test_loss)
         if print_error:
             print(f'Epoch [{epoch + 1}/{epochs}], Train MSE: {train_loss:.4f}, Test MSE: {test_loss:.4f}', )
@@ -140,7 +147,7 @@ def nn_training(model, train_loader, test_loader, epochs, learning_rate, print_e
     return train_loss_list, test_loss_list
 
 
-def make_predict(model, x_train, x_test, is_test = True, window_size = None):
+def make_predict(model, x_train, x_test, is_test = True, window_size = None, DL = False):
     if window_size is None:
         if is_test:
             X = x_test
@@ -197,3 +204,114 @@ def RNN_ensemble(nodes, hidden_size, window_size, model_num, RNN_x_train, RNN_y_
     predictions = np.array(predictions)
     ensemble_prediction = predictions.mean(axis=0)
     return ensemble_prediction, RNN_models
+
+
+def DL_ensemble(kernel_size, individual, window_size, model_num, DL_x_train, DL_y_train, DL_x_test, DL_y_test):
+    DL_models = {}
+    predictions = list()
+    for i in range(model_num):
+        train_loader, test_loader = create_data_loaders(DL_x_train, DL_y_train, DL_x_test, DL_y_test, 128, window_size)
+        DL_models[i] = create_DL(DL_x_train, DL_y_train, window_size, individual, kernel_size)
+        _, _ = nn_training(DL_models[i], train_loader, test_loader, 100, 0.001, print_error = False)
+        prediction = make_predict(DL_models[i], DL_x_train, DL_x_test, True, window_size)
+        predictions.append(prediction)
+    predictions = np.array(predictions)
+    ensemble_prediction = predictions.mean(axis=0)
+    return ensemble_prediction, DL_models
+
+
+
+######################################################################################
+## This part of code comes from (Zeng et al., 2023), it is the arhcitecture of Dlinear
+######################################################################################
+
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
+
+class Dlinear(nn.Module):
+    """
+    Decomposition-Linear
+    """
+    def __init__(self, seq_len, enc_in, pred_len, individual, kernel_size):
+        super(Dlinear, self).__init__()
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+
+        # Decompsition Kernel Size
+        self.kernel_size = kernel_size
+        self.decompsition = series_decomp(self.kernel_size)
+        self.individual = individual
+        self.channels = enc_in
+        
+
+        if self.individual:
+            self.Linear_Seasonal = nn.ModuleList()
+            self.Linear_Trend = nn.ModuleList()
+            
+            for i in range(self.channels):
+                self.Linear_Seasonal.append(nn.Linear(self.seq_len,self.pred_len))
+                self.Linear_Trend.append(nn.Linear(self.seq_len,self.pred_len))
+
+        else:
+            self.Linear_Seasonal = nn.Linear(self.seq_len,self.pred_len)
+            self.Linear_Trend = nn.Linear(self.seq_len,self.pred_len)
+            
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+        seasonal_init, trend_init = self.decompsition(x)
+        seasonal_init, trend_init = seasonal_init.permute(0,2,1), trend_init.permute(0,2,1)
+        if self.individual:
+            seasonal_output = torch.zeros([seasonal_init.size(0),seasonal_init.size(1),self.pred_len],dtype=seasonal_init.dtype).to(seasonal_init.device)
+            trend_output = torch.zeros([trend_init.size(0),trend_init.size(1),self.pred_len],dtype=trend_init.dtype).to(trend_init.device)
+            for i in range(self.channels):
+                seasonal_output[:,i,:] = self.Linear_Seasonal[i](seasonal_init[:,i,:])
+                trend_output[:,i,:] = self.Linear_Trend[i](trend_init[:,i,:])
+        else:
+            seasonal_output = self.Linear_Seasonal(seasonal_init)
+            trend_output = self.Linear_Trend(trend_init)
+
+        x = seasonal_output + trend_output
+        x = x.permute(0,2,1)
+        output = x
+        output = output.squeeze(-1)
+        return  output # to [Batch, Output length, Channel]
+
+
+######################################################################################
+## End of the part ###################################################################
+######################################################################################
+
+def create_DL(x_train, y_train, window_size, individual, kernel_size):
+    DL = Dlinear(window_size, x_train.shape[1], y_train.shape[1], individual, kernel_size)
+    DL = DL.to(DEVICE)
+    return DL
